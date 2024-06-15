f466984e28fe4d465696be07e94c8c8508878be1
ayustinaa <ayustinaintan.2021@student.uny.ac.id>
2024-06-15 12:09:47 +0000

    simpan sementara

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "musical-umbrella-q764w7rj529j7r-3000.app.github.dev"
+  config.hosts << "fantastic-succotash-x59rrw49qvx52prwp-3000.app.github.dev"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'read/:article_id', to: 'home#read', as: "read"
-  post 'save-comment/:article_id', to: 'home#save_comment', as: "save_comment"
-  get 'about', to: 'home#about', as: "about"
-  get 'search', to: 'home#search', as: "search"
+ 
 
+
+  get 'home/index'
   resources :articles
   resources :users
-
   root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
