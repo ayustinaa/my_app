41694db3201cd2ec2d88b36be44de065247ec1e8
ayustinaa <ayustinaintan.2021@student.uny.ac.id>
2024-06-15 15:34:40 +0000

    selesai generate kerangka file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
  
 
 
+  resources :articles
+  resources :users
   root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
